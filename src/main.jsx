2ebe7f84204c9e14529427895e860da4a36ef0f0
anishav473 <you@example.com>
2026-01-03 01:37:05 +0530

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Jagateducation">
     <App />
   </BrowserRouter>
 );
